596b4b0d835b7ac16cf327582c31035c91e4c31c
Mohamed Abo Elfadl <101360482+MohamedAboElfadl-11@users.noreply.github.com>
2025-03-27 00:01:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ import bootstrap from "./src/main.js";
 bootstrap()
 
 
-console.log('helo joo 3')
-console.log("helllllllllllllloooooooooooo jjjjjoooooooooooooooooooooooooo")
-console.log("helllllllllllllloooooooooooo jjjjjoooooooooooooooooooooooooo")
-
